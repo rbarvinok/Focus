bf9d5a989e13dc07e2c652c4f120e11c5fed80ce
Barvinok <rbarvinok@gmail.com>
2021-03-29 10:22:04 +0300

    29032021
    Signed-off-by: Barvinok <rbarvinok@gmail.com>

--- ffmpeg.bat
+++ ffmpeg.bat
@@ -1,3 +1,3 @@
-D:/Focus/ConGoPro/Programs/ffmpeg/bin/ffmpeg.exe -y -i   C:\Users\Barvinok\Documents\VideoGoPro\3333333/GH010489.MP4 -codec copy -map 0:3 -f rawvideo D:/Focus/ConGoPro/transit/GH010489.bin 
-D:/Focus/ConGoPro/Programs/gpmd2csv/gpmd2csv.exe  -i  D:/Focus/ConGoPro/transit/GH010489.bin  -o D:/Focus/ConGoPro/out/GH010489.csv 
+D:/Focus/ConGoPro/Programs/ffmpeg/bin/ffmpeg.exe -y -i   C:\Users\Barvinok\Documents\VideoGoPro/GH010339(12.06.2020).MP4 -codec copy -map 0:3 -f rawvideo D:/Focus/ConGoPro/transit/GH010339(12.06.2020).bin 
+D:/Focus/ConGoPro/Programs/gpmd2csv/gpmd2csv.exe  -i  D:/Focus/ConGoPro/transit/GH010339(12.06.2020).bin  -o D:/Focus/ConGoPro/out/GH010339(12.06.2020).csv 
 exit
